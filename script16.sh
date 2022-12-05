4ab6ceb917a270a5dd042354ca68098f0242dbe6
aws-adrianna <119363996+aws-adrianna@users.noreply.github.com>
2022-12-04 19:44:37 -0800

    Add files via upload

---
+++ script16.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+#######################################################
+#
+#asl user how many users it woud like to create?
+#if more then 20 generats a message
+#
+#
+#
+#
+#######################################################
+
+
+
+
+
+
+
