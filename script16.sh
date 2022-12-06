19a8cfaab1277d1747986d97b155c7ca18e66481
aws-adrianna <119363996+aws-adrianna@users.noreply.github.com>
2022-12-06 08:56:36 -0800

    Add files via upload

--- script16.sh
+++ script16.sh
@@ -1,17 +1,51 @@
 #!/bin/bash 
 #######################################################
-#
-#asl user how many users it woud like to create?
-#if more then 20 generats a message
-#
+#Adrianna
+#Ask users for an ip address
+#checks if private -if not error ,exit.
+#if so ask how many pc?
+#Give ip subenet how ever many times 
 #
 #
 #
 #######################################################
 
+clear
+#ask users for ip address
+echo " hello, Please provide an Ip address "
+
+read numip
+
+#edits the numip input of end user to see only first number in ip address
+range=`echo "$numip" |tr "." " " |awk '{print $1}'`
+
+#number for loop
+a=o
+
+#identifies if range is private and whats next
+if [[ "$range" == "10" ]] || [[ "$range" == "172" ]] || [[ "$range" == "192" ]]
+   then 
+	   echo "How many pc's do you have ?"
+	   read pc
+   else
+	   echo "Error, your IP address is not in a private range"
+	   exit
+
+fi
+
+
+#varaiable to run loop
+a=2
+pc=$(( $pc + 2 ))
+
+#whiles a loop for how ever many users new-gives a subnet
+while [ "$a" -lt "$pc" ]
+do 
+        new=`echo " $numip" > newip |cat newip | cut -c -10` 	
+	echo " $new ${a}"
+	a=$(( $a + 1 ))
+done
 
 
 
-
-
-
+#END
